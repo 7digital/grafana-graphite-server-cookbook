efbb4739facdd285ea86002105088a31fd26ebf8
Darrell Mozingo <darrell@mozingo.net>
2015-09-18 17:16:56 +0100

    Ping port 8125 for statsd.

--- recipes/graphite.rb
+++ recipes/graphite.rb
@@ -2,4 +2,5 @@ docker_image 'hopsoft/graphite-statsd'
 
 docker_container 'graphite' do
   repo 'hopsoft/graphite-statsd'
+  port '8125:8125/udp'
 end
--- test/integration/linux/serverspec/graphite_spec.rb
+++ test/integration/linux/serverspec/graphite_spec.rb
@@ -8,5 +8,8 @@ describe 'graphite' do
 
   describe docker_container('graphite') do
     it { should be_running }
+    its(['HostConfig.PortBindings']) { should include '8125/udp' }
+    #Enable when this PR is merged: https://github.com/mizzy/serverspec/pull/536
+    #its(['HostConfig.PortBindings.8125/udp.[0].HostPort']) { should eq '8125' }
   end
 end
