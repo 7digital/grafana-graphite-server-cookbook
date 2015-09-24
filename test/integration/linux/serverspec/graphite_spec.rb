f31459ee5a89dce75b628f0ca0410e9958028f46
Darrell Mozingo <darrell@mozingo.net>
2015-09-24 18:15:14 +0100

    Version bump with my PR in serverspec merged.

--- test/integration/linux/serverspec/graphite_spec.rb
+++ test/integration/linux/serverspec/graphite_spec.rb
@@ -9,7 +9,6 @@ describe 'graphite' do
   describe docker_container('graphite') do
     it { should be_running }
     its(['HostConfig.PortBindings']) { should include '8125/udp' }
-    #Enable when this PR is merged: https://github.com/mizzy/serverspec/pull/536
-    #its(['HostConfig.PortBindings.8125/udp.[0].HostPort']) { should eq '8125' }
+    its(['HostConfig.PortBindings.8125/udp.[0].HostPort']) { should eq '8125' }
   end
 end
