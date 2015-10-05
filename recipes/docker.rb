accd3d2e4c89f51152ecbe167bb298973e7dbf04
Darrell Mozingo <darrell@mozingo.net>
2015-10-05 18:20:22 +0100

    Added a note about why we're on Docker 1.7.1 for now

--- recipes/docker.rb
+++ recipes/docker.rb
@@ -1,5 +1,5 @@
 docker_service 'default' do
-  version '1.7.1'
+  version '1.7.1' # ServerSpec doesn't fully support Docker 1.8+ yet (different 'docker inspect' response)
   provider Chef::Provider::DockerService::Systemd
   action [:create, :start]
 end
