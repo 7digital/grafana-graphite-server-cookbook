a7a60da97285f6cfd22b1cdd0c994414562132f6
Darrell Mozingo <darrell@mozingo.net>
2015-10-21 14:27:46 +0100

    Pulled path to a variable.

--- recipes/graphite.rb
+++ recipes/graphite.rb
@@ -1,21 +1,23 @@
-directory '/opt/docker-images/graphite' do
+image_build_folder = '/opt/docker-images/graphite'
+
+directory image_build_folder do
   recursive true
 end
 
-cookbook_file '/opt/docker-images/graphite/Dockerfile' do
+cookbook_file "#{image_build_folder}/Dockerfile" do
   source 'Dockerfile.graphite'
 end
 
-remote_directory '/opt/docker-images/graphite/graphite-configs' do
+remote_directory "#{image_build_folder}/graphite-configs" do
   source 'graphite-configs'
 end
 
-remote_directory '/opt/docker-images/graphite/statsd-configs' do
+remote_directory "#{image_build_folder}/statsd-configs" do
   source 'statsd-configs'
 end
 
 docker_image '7d-graphite-statsd' do
-  source '/opt/docker-images/graphite'
+  source image_build_folder
   nocache true
   #todo: tag w/the cookbook version & use below?
   action :build_if_missing
