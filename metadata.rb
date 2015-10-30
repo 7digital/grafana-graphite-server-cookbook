9a0182ee05bc49e5d4bbe350592960fd1dc26b42
Darrell Mozingo <darrell@mozingo.net>
2015-10-30 17:55:48 +0000

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'content-discovery-devteam@7digital.com'
 license          'All rights reserved'
 description      'Installs Grafana & Graphite'
 long_description 'Runs them via Docker'
-version          '1.0.0'
+version          '1.1.0'
 
 supports 'debian'
 
