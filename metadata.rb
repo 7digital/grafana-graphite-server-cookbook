019947f60cfb40f79e79e4d91eefa0e8bee214e8
Darrell Mozingo <darrell@mozingo.net>
2015-11-27 14:41:22 +0000

    Version to 2.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tech-team@7digital.com'
 license 'MIT'
 description 'Installs Grafana & Graphite'
 long_description 'Runs them via Docker'
-version '2.1.0'
+version '2.2.0'
 
 supports 'debian'
 
